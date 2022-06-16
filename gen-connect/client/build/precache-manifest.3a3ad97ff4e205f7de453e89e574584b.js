3df30ff8d1c0f2058530e1d23f3256506df30dcd
Pooja Rathore <poojarathore.nks@gmail.com>
2022-06-16 22:32:56 +0530

    Initial

---
+++ gen-connect/client/build/precache-manifest.3a3ad97ff4e205f7de453e89e574584b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b69af8753e6b07cac080bc8ffdf8f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a62194ed768faeb2598c",
+    "url": "/static/css/main.338d8e1d.chunk.css"
+  },
+  {
+    "revision": "4c3d300390d79f67d6ce",
+    "url": "/static/js/2.5a1818ed.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.5a1818ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62194ed768faeb2598c",
+    "url": "/static/js/main.5bc00c04.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3bdb36c11a882c19981d5d2b44259579",
+    "url": "/static/media/YG.3bdb36c1.png"
+  },
+  {
+    "revision": "1fa6ecb807e693badb0175d844312533",
+    "url": "/static/media/new.1fa6ecb8.jpeg"
+  },
+  {
+    "revision": "0b4e28dae1aa3b0826bc290aad39b797",
+    "url": "/static/media/welcome.0b4e28da.PNG"
+  }
+]);
